5a1e544524d4f6f8824ac35cf2d9b4e48f4e1952
David Wade <dawad@equinor.com>
2020-07-20 11:02:19 +0000

    Changed to CMake

--- zgy2sgz/setup.py
+++ zgy2sgz/setup.py
@@ -5,7 +5,7 @@ ext_modules = [Extension("zgy2sgz",
                          sources=["zgy2sgz.pyx"],
                          libraries=["zgy2sgz"],
                          extra_compile_args=["-I../open-zgy/zgy/Slb.Salmon.ZgyPublic/include/"],
-                         extra_link_args=["-L."],
+                         extra_link_args=["-L./build"],
                          language='c++',)]
 
 setup(name='zgy2sgz', cmdclass={'build_ext': build_ext}, ext_modules=ext_modules)
