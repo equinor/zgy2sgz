afe61c99156edce37ab8ee91bca0ae4d165ad279
dawad <dawad@statoil.com>
2021-06-14 09:11:45 +0200

    Add Python 3.9 support, remove Python 3.5 support

--- config.sh
+++ config.sh
@@ -10,7 +10,10 @@ function pre_build {
 
     # Need to tell auditwheel where to find libzfp
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/zfp/build/lib64/:$(pwd)/open-zgy/zgy/Slb.Salmon.ZgyPublic/Lin64_gcc485:$(pwd)/zgy2sgz/build/convert
-    $PYTHON_EXE -m pip install "cmake<3.14"
+    $PYTHON_EXE -m pip install \
+    "cmake < 3.14; python_version < '3.8'" \
+    "cmake; python_version >= '3.8'"
+
 
     ZFP_BUILD_DIR="zfp/build"
     BUILD_DIR="zgy2sgz/build"
